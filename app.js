a88640c3e6592a9bc4bce4c882e561b9fc9b3c8d
xiaoxi <398670954@qq.com>
2019-07-02 20:03:57 +0800

    save

--- app.js
+++ app.js
@@ -33,12 +33,57 @@ App({
   },
   /** 全局变量 */
   globalData:{
+    isDebug:true,
+    xdebug:"?XDEBUG_SESSION_START=12050",
     domain:'https://www.yunxiaozhi.cn/v1/public/api/',
     key:'ihzoaixnuy4f8835032505e8a45ac102c52d58593e',
     start_year: 2019,
     start_month: 2,
     start_day: 25,
   },
+
+  /**
+   * 获取请求domain
+   */
+  getDomain:function(){
+    return this.globalData.isDebug ? 'https://danbaixi.utools.club/yxz_v1/public/api/' : 'https://www.yunxiaozhi.cn/v1/public/api/'
+  },
+
+  /**
+   *  封装request 
+   */
+  httpRequest:function(datas){
+    var that = this,
+        isDebug = this.globalData.isDebug,
+        domain = this.getDomain(),
+        contentType = 'application/json'
+
+    if (typeof datas.method == undefined){
+      datas.method = "GET"
+    }
+    if(datas.method == "POST"){
+      contentType = 'application/x-www-form-urlencoded'
+    }
+    wx.request({
+      url: domain + datas.url + (isDebug?this.globalData.xdebug:""),
+      data: typeof datas.data == undefined ? '': datas.data,
+      method: datas.method,
+      header: {
+        'content-type': contentType
+      },
+      success:function(res){
+        datas.success(res)
+      },
+      fail:function(res){
+        wx.showToast({
+          title: '请求超时',
+          icon:'none',
+          duration:2000
+        })
+      }
+    })
+  },
+
   //自定义Toast
   showToast: function (text, o, count) {
     var _this = o;
@@ -53,4 +98,13 @@ App({
       });
     }, count);
   },
+
+  //toast
+  msg:function(text){
+    wx.showToast({
+      title: text,
+      icon:'none',
+      duration:2000
+    })
+  }
 })
--- pages/bind/bind.js
+++ pages/bind/bind.js
@@ -1,6 +1,5 @@
 var util = require('../../utils/util.js');
 var md5 = require('../../utils/md5.js');
-const { $Toast } = require('../../dist/base/index');
 var app = getApp();
 Page({
 
@@ -8,24 +7,24 @@ Page({
    * 页面的初始数据
    */
   data: {
-    yzmUrl: "", 
+    yzmUrl: "",
     cookie: "",
-    cookie_2:"",
+    cookie_2: "",
     __VIEWSTATE: "",
     user_id: "",
     user_password: "",
-    code: "", 
-    password_display:false,
-    loading:false,
-    modal_visible:false,
-    hasNotice:false,
-    systemType:2
+    code: "",
+    password_display: false,
+    loading: false,
+    modal_visible: false,
+    hasNotice: false,
+    systemType: 2
   },
 
   /**
-   * 生命周期函数--监听页面加载
+   * 生命周期函数--监听页面 加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     if (wx.getStorageSync('user_id')) {
       wx.redirectTo({
         url: '../course/course',
@@ -33,52 +32,53 @@ Page({
     }
     var that = this;
     that.getNotice();
-    that.loginInitV1(); 
-    wx.request({
-      url: app.globalData.domain + 'login/getLoginCookie',
-      success: function (res) {
-        if(res.data.status == 1002){
-          $Toast({ content: res.data.message, type: 'error' })
-          return
-        }
-        that.setData({
-          cookie: res.data.data.cookie,
-        })
-      }
-    });
+    that.loginInitV1();
+
+    //新教务系统获取cookie，暂时隐藏
+    // app.httpRequest({
+    //   url: 'login/getLoginCookie',
+    //   data: '',
+    //   success: function(res) {
+    //     that.setData({
+    //       cookie: res.data.data.cookie,
+    //     })
+    //   }
+    // })
   },
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   },
 
   /**
    * 绑定教务系统
    */
-  login: function (e) {
+  login: function(e) {
     var that = this;
     var user_id = that.data.user_id;
     var password = that.data.user_password;
     var code = that.data.code;
     if (user_id == "") {
-      $Toast({ content: '请输入学号', type:'warning'});
+      app.msg('请输入学号')
       return
     }
     if (password == "") {
-      $Toast({ content: '请输入密码', type: 'warning' })
-      return
-    } 
-    if (that.data.systemType==2 && code == ""){
-      $Toast({ content: '请输入验证码', type: 'warning' });
+      app.msg('请输入密码')
       return
     }
-    $Toast({ content: '登录中', type: 'loading', duration: 0 });
-    if(that.data.systemType == 1){
+    if (that.data.systemType == 2 && code == "") {
+      app.msg('请输入验证码')
+      return
+    }
+    wx.showLoading({
+      title: '登陆中...',
+    })
+    if (that.data.systemType == 1) {
       var cookie = that.data.cookie;
       var encoded = util.encodeInp(user_id) + "%%%" + util.encodeInp(password);
-      wx.request({
+      app.httpRequest({
         url: app.globalData.domain + 'login/Login',
         data: {
           stu_id: user_id,
@@ -87,11 +87,8 @@ Page({
           encoded: encoded,
         },
         method: "POST",
-        header: {
-          'content-type': 'application/x-www-form-urlencoded'
-        },
         success: function (res) {
-          $Toast.hide();
+          wx.hideLoading()
           if (res.data.status == 1002) {
             var str = app.globalData.key + user_id;
             var sign = md5.hexMD5(str);
@@ -105,7 +102,10 @@ Page({
                 if (res.data.status == 1001) {
                   wx.setStorageSync('course', res.data.data.course);
                 } else {
-                  $Toast({ content: '获取课表失败', type: 'error' });
+                  $Toast({
+                    content: '获取课表失败',
+                    type: 'error'
+                  });
                 }
               },
             });
@@ -115,19 +115,30 @@ Page({
             wx.setStorageSync('user_new_password', password);
             wx.setStorageSync('system_type', that.data.systemType);
             setTimeout(function () {
-              wx.switchTab({ url: '../index/index' })
+              wx.switchTab({
+                url: '../index/index'
+              })
             }, 1000);
-            $Toast({ content: '登录成功', type: 'success' });
+            $Toast({
+              content: '登录成功',
+              type: 'success'
+            });
           } else if (res.data.status == 1003) {
-            $Toast({ content: '学号或密码有误', type: 'error' });
+            $Toast({
+              content: '学号或密码有误',
+              type: 'error'
+            });
           } else {
-            $Toast({ content: '登录失败', type: 'error' });
+            $Toast({
+              content: '登录失败',
+              type: 'error'
+            });
           }
         }
       })
-    }else{
-      wx.request({
-        url: app.globalData.domain + 'login/LoginV1',
+    } else {
+      app.httpRequest({
+        url: 'login/LoginV1',
         data: {
           stu_id: user_id,
           password: password,
@@ -136,122 +147,106 @@ Page({
           code: that.data.code,
         },
         method: "POST",
-        header: {
-          'content-type': 'application/x-www-form-urlencoded'
-        },
-        success: function (res) {
-          $Toast.hide();
-          if (res.data.status == 1002) {
-            var str = app.globalData.key + user_id;
-            var sign = md5.hexMD5(str);
+        success: function(res) {
+          wx.hideLoading()
+          if (res.data.status == 0) {
+            wx.showToast({
+              title: '登录成功',
+              icon:'success'
+            })
             wx.setStorageSync('user_id', user_id);
-            wx.setStorageSync('user_new_password', res.data.data);
-            wx.setStorageSync('user_old_password', password);
             wx.setStorageSync('user_password', password);
             wx.setStorageSync('system_type', that.data.systemType);
-            wx.request({
-              url: app.globalData.domain + 'course/getList',
-              data: {
-                stu_id: user_id,
-                sign: sign
-              },
-              success: function (res) {
-                if (res.data.status == 1001) {
-                  $Toast({ content: '登录成功', type: 'success' });
-                  wx.setStorageSync('course', res.data.data.course);
-                  wx.setStorageSync('train', res.data.data.train_course);
-                  wx.switchTab({ url: '../index/index' })
-                } else if(res.data.status == 1002){
-                  $Toast({ content: '登录成功', type: 'success' });
-                  wx.switchTab({ url: '../index/index' })
-                }else{
-                  $Toast({ content: '登录失败', type: 'error' });
-                }
-              },
-            }); 
-          }else {
-            $Toast({ content: '登录失败', type: 'error' });
-            that.freshYzm();
+            setTimeout(function(){
+              wx.switchTab({
+                url: '../index/index'
+              })
+            },1000)
+          } else {
+            app.msg(res.data.message);
+            if(res.data.status == 1001){
+              that.freshYzm();
+            }
           }
         }
       })
     }
-    
+
 
   },
   /** 刷新验证码 */
-  freshYzm: function () {
+  freshYzm: function() {
     var num = Math.ceil(Math.random() * 1000000);
     this.setData({
-      loading:true,
-      yzmUrl: app.globalData.domain + 'login/getValidateImg?cookie=' + this.data.cookie_2 + '&rand=' + num,
+      loading: true,
+      yzmUrl: app.getDomain() + 'login/getValidateImg?cookie=' + this.data.cookie_2 + '&rand=' + num,
     })
   },
   /** 输入学号 */
-  inputUserId:function(e){
+  inputUserId: function(e) {
     var that = this;
     that.setData({
       user_id: e.detail.value.trim()
     })
   },
   /** 输入密码 */
-  inputPwd: function (e) {
+  inputPwd: function(e) {
     var that = this;
     that.setData({
       user_password: e.detail.value
     })
   },
   /** 输入验证码 */
-  inputCode: function (e) {
+  inputCode: function(e) {
     var that = this;
     that.setData({
       code: e.detail.value
     })
   },
   /** 清空学号 */
-  clearUserId:function(){
+  clearUserId: function() {
     this.setData({
-      user_id:''
+      user_id: ''
     })
   },
   /** 展开密码 */
-  displayPwd:function(){
+  displayPwd: function() {
     this.setData({
-      display_password:true
+      display_password: true
     })
   },
   /** 隐藏密码 */
-  hidePwd: function () {
+  hidePwd: function() {
     this.setData({
       display_password: false
     })
   },
   /** 图片预加载 */
-  imageLoad:function(){
+  imageLoad: function() {
     this.setData({
-      loading:false
+      loading: false
     })
   },
   /** 帮助 */
-  help:function(){
+  help: function() {
     this.setData({
-      modal_visible:true,
+      modal_visible: true,
     })
   },
-  handleClose:function(){
+  handleClose: function() {
     this.setData({
       modal_visible: false,
     })
   },
   //获取公告
-  getNotice: function () {
+  getNotice: function() {
     var that = this;
     wx.request({
       url: app.globalData.domain + 'notice/getnotice',
       data: {
         page: 'login'
       },
-      success: function (res) {
+      success: function(res) {
         if (res.data.status == 1001) {
           that.setData({
             hasNotice: res.data.data.display == 1 ? true : false,
@@ -267,22 +262,22 @@ Page({
   },
 
   //切换登录方式
-  changeLoginType:function(e){
+  changeLoginType: function(e) {
     var type = e.target.dataset.type;
     this.setData({
-      systemType:type
+      systemType: type
     })
-    if(this.data.cookie_2 == ""){
-      this.loginInitV1(); 
+    if (this.data.cookie_2 == "") {
+      this.loginInitV1();
     }
 
   },
 
   //获取旧的教务系统登录cookie
-  loginInitV1:function(){
+  loginInitV1: function() {
     var that = this
-    wx.request({
-      url: app.globalData.domain + 'login/getLoginInitData',
+    app.httpRequest({
+      url: 'login/getLoginInitData',
       success: function (res) {
         that.setData({
           cookie_2: res.data.data.cookie,
@@ -290,6 +285,6 @@ Page({
         })
         that.freshYzm();
       }
-    });
+    })
   }
 })
