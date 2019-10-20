c1b854086355ce23262aa6b21a4aca69df9c04ae
xiaoxi <398670954@qq.com>
2019-10-21 07:55:32 +0800

    优化

--- pages/index/index.js
+++ pages/index/index.js
@@ -97,21 +97,6 @@ Page({
   },
 
   onLoad: function () {
-    const updateManager = wx.getUpdateManager()
-    updateManager.onCheckForUpdate(function (res) {
-    })
-    updateManager.onUpdateReady(function () {
-      wx.showModal({
-        title: '更新提示',
-        content: '新版本已经准备好，请重启应用',
-        success: function (res) {
-          if (res.confirm) {
-            updateManager.applyUpdate()
-          }
-        }
-      })
-    })
-
     if(wx.getStorageSync('showRedDot') != 1){
       wx.showTabBarRedDot({
         index: 2
