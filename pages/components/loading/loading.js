880cbe0ebe89d08b7a261571adf6248f15b40cb5
xiaoxi <398670954@qq.com>
2019-10-23 08:52:08 +0800

    优化加载

---
+++ pages/components/loading/loading.js
@@ -0,0 +1,26 @@
+// pages/components/loading/loading.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    loading:{
+      type: Boolean,
+      value: true
+    }
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
