7ad1c885ab8f26aa4896d2e782e80f972098705a
xiaoxi <398670954@qq.com>
2020-06-29 17:49:15 +0800

    增加查题功能

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ App({
   },
   /** 全局变量 */
   globalData:{
-    isDebug:false,
+    isDebug:true,
     isTest:false,
     isLocal:false,
     themeColor: '#1380ff',
@@ -75,7 +75,7 @@ App({
    * 获取请求domain
    */
   getDomain:function(){
-    return this.globalData.isDebug ? (this.globalData.isTest ? 'https://www.yunxiaozhi.cn/test/public/api/' : 'https://danbaixi.utools.club/yxz_v1/public/index.php/api/') : 'https://www.yunxiaozhi.cn/v1/public/api/'
+    return this.globalData.isDebug ? (this.globalData.isTest ? 'https://www.yunxiaozhi.cn/test/public/api/' : 'http://localhost/yxz_v1/public/index.php/api/') : 'https://www.yunxiaozhi.cn/v1/public/api/'
   },
 
   getSign:function(){
---
+++ pages/tools/question/question.js
@@ -0,0 +1,148 @@
+const app = getApp()
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    loading: false,
+    display: true,
+    clip: true,
+    auto: false
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    app.isLogin()
+    this.init()
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    this.getClip()
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+  
+  //初始化
+  init:function(){
+    let _this = this
+    app.httpRequest({
+      url:'question/init',
+      success:function(res){
+        if(res.data.status == -1){
+          app.msg(res.data.status)
+          _this.setData({
+            display: false
+          })
+          return
+        }
+        let display = true
+        if(res.data.data.notice != ''){
+          display = false
+        }
+        res.data.data.display = display
+        _this.setData(res.data.data)
+      }
+    })
+  },
+  //看广告增加次数
+  add:function(){
+    this.addCount()
+  },
+  //增加次数
+  addCount:function(){
+    let _this = this
+    app.httpRequest({
+      url:'question/addCount',
+      success:function(res){
+        app.msg(res.data.message)
+        if(res.data.status == 0){
+          _this.setData(res.data.data)
+        }
+      }
+    })
+  },
+  //自动获取开关
+  switchClip:function(e){
+    this.setData({
+      clip: e.detail.value
+    })
+  },
+  //自动查询开关
+  switchAuto:function(e){
+    this.setData({
+      auto: e.detail.value
+    })
+  },
+  //获取剪贴板内容
+  getClip:function(){
+    if(!this.data.clip){
+      return
+    }
+    let _this = this
+    wx.getClipboardData({
+      success: (res) => {
+        _this.setData({
+          question:res.data
+        })
+      },
+      fail:(res) => {
+        console.log('获取剪贴板内容失败')
+      }
+    })
+  },
+  //查题
+  query:function(){
+
+  },
+  //请求
+  apiQuery:function(){
+
+  },
+
+})
