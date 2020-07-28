57a0aa7b8a14d48bfa1109da310a196795d17aa2
xiaoxi <398670954@qq.com>
2020-07-28 17:28:11 +0800

    录取查询

--- pages/tools/matriculate/matriculate.js
+++ pages/tools/matriculate/matriculate.js
@@ -5,6 +5,7 @@ Page({
    * 页面的初始数据
    */
   data: {
+    tzs:"http://mp.weixin.qq.com/s?__biz=MzI1NTUwNDIzNQ==&mid=100002372&idx=1&sn=c6d1d66a075b36c7487cfac12f97502c&chksm=6a35b0405d423956885847d84eb6091ef01802c17d12ec411af4a1fc05e62580a4a18a9b103d#rd",
     type : 0,
     list:[
       {
@@ -16,6 +17,11 @@ Page({
         title:'专插本',
         img: 'zcb',
         color:'yellow'
+      },
+      {
+        title:'录取通知书查询',
+        img: 'zcb',
+        color:'cyan'
       },
     ]
   },
@@ -75,8 +81,15 @@ Page({
   onShareAppMessage: function () {
 
   },
-  selectType:function(){
-    app.msg("即将上线，尽请期待")
-    return
+  selectType:function(e){
+    let index = e.currentTarget.dataset.index
+    if(index == 2){
+      wx.navigateTo({
+        url: '/pages/article/article?src=' + encodeURIComponent(this.data.tzs),
+      })
+    }else{
+      app.msg("即将上线，尽请期待")
+      return
+    }
   }
 })
