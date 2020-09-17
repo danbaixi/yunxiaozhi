596a68f0961486a91e2204db1f1764f754d7f04b
xiaoxi <398670954@qq.com>
2020-09-17 17:55:57 +0800

    运动

--- pages/tools/sport/detail/detail.js
+++ pages/tools/sport/detail/detail.js
@@ -9,7 +9,8 @@ Page({
         weeks: ['MON','TUE','WEB','THU','FRI','SAT','SUN'],
         year: 2020,
         month: 1,
-        days:[]
+        days:[],
+        thisMonth:true
     },
 
     /**
@@ -19,7 +20,7 @@ Page({
         let date = new Date()
         this.setData({
             year: date.getFullYear(),
-            month: date.getMonth() + 1,
+            month: this.getMonth(date.getMonth() + 1),
             day: date.getDate()
         })
         this.getDays()
@@ -79,21 +80,32 @@ Page({
         let dayStart = util.getFirstDayOfWeek(this.data.year,this.data.month)
         let days = new Array(len+dayStart-1).fill(0)
         let num = 1
+        let fillCount = 7 - ((len + dayStart) % 7) + 1
         for(let i=dayStart-1;i<days.length;i++){
             if(num < 10){
-            days[i] = '0' + num
+                days[i] = '0' + num
             }else{
-            days[i] = '' + num
+                days[i] = '' + num
             }
             num++
         }
+        for(let i=1;i<=fillCount;i++){
+            days.push(0)
+        }
         this.setData({
-            days:days
+            days:days,
+            lineCount: Math.ceil(days.length / 7)
         })
+    },
+    getMonth:function(month){
+        return month > 9 ? month : '0' + month
     },
     //获取数据
     getData:function(){
         let _this = this
+        wx.showLoading({
+          title: '加载中',
+        })
         app.httpRequest({
             url: 'sport/getDetailForMonth',
             data:{
@@ -101,12 +113,48 @@ Page({
                 month: _this.data.month
             },
             success:function(res){
+                wx.hideLoading({
+                  success: (res) => {},
+                })
                 if(res.data.status != 0){
                     app.msg(res.data.message)
                     return
                 }
                 _this.setData(res.data.data)
             }
+        })
+    },
+    setMonth:function(e){
+        let year = this.data.year
+        let month = this.data.month
+        let type = e.currentTarget.dataset.type
+        if(type == 'next'){
+          month++
+        }else{
+          month--
+        }
+        if(month > 12){
+          year++
+          month = 1
+        }
+        if(month < 1){
+          year--
+          month = 12
+        }
+        month = this.getMonth(month)
+        this.setData({
+          year:year,
+          month:month,
+          list:[]
+        })
+        let thisMonth = false
+        if(year == new Date().getFullYear && month == new Date().getMonth + 1){
+            thisMonth = true
+        }
+        this.getDays()
+        this.getData()
+        this.setData({
+          thisMonth: thisMonth
         })
     }
 })
--- pages/tools/sport/sport.js
+++ pages/tools/sport/sport.js
@@ -16,6 +16,7 @@ Page({
    */
   onLoad: function (options) {
     this.init()
+    this.getData()
   },
 
   /**
