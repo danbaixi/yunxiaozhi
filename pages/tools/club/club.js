247394684f60ff100dcc026a3598b0332e703de0
“danbaixi <398670954@qq.com>
2022-09-06 22:52:14 +0800

    style:优化社团页面样式

--- pages/tools/club/club.js
+++ pages/tools/club/club.js
@@ -1,5 +1,9 @@
 const app = getApp()
-const { getClubList, getClubCategory,starClub } = require('../../api/other')
+const {
+  getClubList,
+  getClubCategory,
+  starClub
+} = require('../../api/other')
 Page({
 
   /**
@@ -7,18 +11,18 @@ Page({
    */
   data: {
     customBar: app.globalData.customBar,
-    loading:false,
-    list:[],
-    p:1,
-    length:10,
+    loading: false,
+    list: [],
+    p: 1,
+    length: 10,
     search: '',
     oldSearch: '',
     notMore: false,
-    category:[{
-      id:0,
-      name:"全部分类"
+    category: [{
+      value: 0,
+      text: "全部分类"
     }],
-    cid:0,
+    cid: 0
   },
 
   /**
@@ -33,7 +37,7 @@ Page({
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    if(!this.data.notMore){
+    if (!this.data.notMore) {
       this.setData({
         loading: true,
         p: this.data.p + 1
@@ -52,42 +56,42 @@ Page({
     }
   },
 
-  searchInput:function(e){
+  searchInput: function (e) {
     this.setData({
       search: e.detail.value
     })
   },
-  search:function(){
-    if(this.data.search == this.data.oldSearch){
+  search: function () {
+    if (this.data.search == this.data.oldSearch) {
       return
     }
     this.setData({
       oldSearch: this.data.search,
       list: [],
-      p:1
+      p: 1
     })
     this.getList()
   },
 
   // 获取列表
-  getList:function(){
-    let _this = this 
+  getList: function () {
+    let _this = this
     let stu_id = wx.getStorageSync('user_id')
     _this.setData({
       loading: true
     })
     getClubList({
-      search:_this.data.search,
-      cid: _this.data.category[_this.data.cid].id,
+      search: _this.data.search,
+      cid: _this.data.category[_this.data.cid].value,
       p: _this.data.p,
       length: _this.data.length,
-      stu_id:stu_id
+      stu_id: stu_id
     }).then((res) => {
-      if(res.status == 0){
+      if (res.status == 0) {
         let list = _this.data.list
-        if(res.data.list.length < _this.data.length){
+        if (res.data.list.length < _this.data.length) {
           res.data.notMore = true
-        }else{
+        } else {
           res.data.notMore = false
         }
         list = list.concat(res.data.list)
@@ -98,12 +102,18 @@ Page({
   },
 
   //获取分类
-  getCategory:function(){
+  getCategory: function () {
     let _this = this
     getClubCategory().then((res) => {
-      if(res.status == 0){
+      if (res.status == 0) {
+        const list = res.data.map(item => {
+          return {
+            value: item.id,
+            text: item.name
+          }
+        })
         let category = _this.data.category
-        category = category.concat(res.data)
+        category = category.concat(list)
         _this.setData({
           category: category
         })
@@ -112,21 +122,21 @@ Page({
   },
 
   //选择分类
-  selectCategory:function(e){
-    let select = e.detail.value
-    if(select == this.data.cid){
+  selectCategory: function (e) {
+    let select = e.detail
+    if (select == this.data.cid) {
       return
     }
     this.setData({
-      p:1,
-      list:[],
-      cid:select
+      p: 1,
+      list: [],
+      cid: select
     })
     this.getList()
   },
 
   // 详情
-  showItem:function(e){
+  showItem: function (e) {
     let index = e.currentTarget.dataset.index
     let club = this.data.list[index]
     wx.navigateTo({
@@ -135,24 +145,24 @@ Page({
   },
 
   //点赞
-  star:function(e){
+  star: function (e) {
     let _this = this
     let stu_id = wx.getStorageSync('user_id')
     let index = e.currentTarget.dataset.index
     let cid = _this.data.list[index].id
-    if(stu_id == ''){
+    if (stu_id == '') {
       app.msg("登录后才能点赞哦")
       return
     }
-    if(_this.data.list[index].stared == 1){
+    if (_this.data.list[index].stared == 1) {
       app.msg("你已经点过赞啦！")
       return
     }
     starClub({
-      cid:cid,
-      stu_id:stu_id
+      cid: cid,
+      stu_id: stu_id
     }).then((res) => {
-      if(res.status == 0){
+      if (res.status == 0) {
         app.msg(res.message)
         let list = _this.data.list
         list[index].star++
