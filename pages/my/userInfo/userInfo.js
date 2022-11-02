b74cd5c26983bf3f499e126576017f241746de03
江济溪 <398670954@qq.com>
2022-11-02 17:08:31 +0800

    feat:优化修改头像和默认头像显示

--- pages/my/userInfo/userInfo.js
+++ pages/my/userInfo/userInfo.js
@@ -1,4 +1,9 @@
-const { getUserData, setNickname, setAvatar,updateUserInfo } = require('../../api/user')
+const {
+  getUserData,
+  setNickname,
+  setAvatar,
+  updateUserInfo
+} = require('../../api/user')
 const app = getApp()
 const dayjs = require('../../../utils/dayjs.min')
 Page({
@@ -6,12 +11,12 @@ Page({
   /**
    * 页面的初始数据
    */
-  data: { 
-    user_img:'http://yunxiaozhi-1251388077.cosgz.myqcloud.com/user_imgs/defalut.png',
+  data: {
+    user_img: 'https://mmbiz.qpic.cn/mmbiz_png/YWKTC18p77L6xvSibXwLNRwQNNLNtOfCjCCicib4hy5FvvQy0T0QictCPTf4RwSVUaLFYeN17lxicc7wQLGH8QxVKdw/0?wx_fmt=png',
     fileUrl: 'http://yunxiaozhi-1251388077.cosgz.myqcloud.com/user_imgs/',
-    loading:true,
-    uploadFile:'',
-    nickNameInput:''
+    loading: true,
+    uploadFile: '',
+    nickNameInput: ''
   },
 
   /**
@@ -20,15 +25,17 @@ Page({
   onLoad: function (options) {
     this.getInfo()
   },
-  onShow: function(){
-    if(this.data.uploadFile != ''){
+  onShow: function () {
+    if (this.data.uploadFile != '') {
       //设置头像
       let _this = this
-      setAvatar({url: _this.data.uploadFile}).then((res) => {
+      setAvatar({
+        url: _this.data.uploadFile
+      }).then((res) => {
         app.msg(res.message)
-        if(res.status == 0){
+        if (res.status == 0) {
           _this.setData({
-            user_img : _this.data.fileUrl + _this.data.uploadFile
+            user_img: _this.data.fileUrl + _this.data.uploadFile
           })
         }
       })
@@ -37,7 +44,7 @@ Page({
   onShareAppMessage: function () {
     return app.share()
   },
-  getInfo:function(){
+  getInfo: function () {
     var that = this;
     getUserData().then((result) => {
       let data = result.data
@@ -46,39 +53,49 @@ Page({
       //时间戳转换
       var date = new Date(parseInt(data.user_regTime) * 1000);
       var regTime = dayjs(date).format('YYYY-MM-DD')
-      var xueji = [
-        { 'title': '姓名', 'data': data.name },
-        { 'title': '年级', 'data': data.stu_schoolday },
-        { 'title': '学号', 'data': wx.getStorageSync('user_id') },
-        { 'title': '学院', 'data': data.stu_department },
-        { 'title': '班级', 'data': stu_class },
-        { 'title': '注册时间', 'data': regTime },
+      var xueji = [{
+          'title': '姓名',
+          'data': data.name
+        },
+        {
+          'title': '年级',
+          'data': data.stu_schoolday
+        },
+        {
+          'title': '学号',
+          'data': wx.getStorageSync('user_id')
+        },
+        {
+          'title': '学院',
+          'data': data.stu_department
+        },
+        {
+          'title': '班级',
+          'data': stu_class
+        },
+        {
+          'title': '注册时间',
+          'data': regTime
+        },
       ];
       that.setData({
-        user_img: data.user_img ? that.data.fileUrl + data.user_img : (data.avatar ? data.avatar : that.data.fileUrl + 'default.png'),
+        user_img: data.user_img ? that.data.fileUrl + data.user_img : (data.avatar ? data.avatar : that.data.user_img),
         user_name: data.user_name,
         xueji: xueji,
-        nickname:data.nickname,
-        avatar:data.avatar,
+        nickname: data.nickname,
+        avatar: data.avatar,
         loading: false
       });
-      
+
     }).catch((message) => {
       app.msg(message)
     })
   },
   //更新头像
-  updateImg:function(){
-    wx.chooseImage({
-      count: 1,
-      sizeType: ['compressed'],
-      sourceType: ['album', 'camera'],
-      success: function (res) {
-        const src = res.tempFilePaths[0]
-        wx.navigateTo({
-          url: `../../cropper/cropper?&type=avatar&src=${src}&width=400&height=400`
-        })
-      }
+  updateImg: function (e) {
+    const src = e.detail.avatarUrl
+    wx.navigateTo({
+      url: `../../cropper/cropper?&type=avatar&src=${src}&width=400&height=400`
     })
   },
   /**
@@ -103,7 +120,7 @@ Page({
   },
 
   //修改昵称窗口打开
-  openUpdateName:function(){
+  openUpdateName: function () {
     var that = this;
     that.setData({
       showModal: true,
@@ -132,10 +149,10 @@ Page({
   },
 
   // 修改昵称
-  updateName:function(e){
+  updateName: function (e) {
     var that = this;
     var nickName = that.data.nickNameInput;
-    if(nickName == '' || nickName == that.data.user_name){
+    if (nickName == '' || nickName == that.data.user_name) {
       app.msg("没有修改内容")
       return
     }
@@ -143,29 +160,31 @@ Page({
       title: '提交中',
       mask: true
     })
-    setNickname({nickname: nickName}).then((data) => {
+    setNickname({
+      nickname: nickName
+    }).then((data) => {
       that.hideModal();
       app.msg(data.message)
-      if(data.status == 0){
+      if (data.status == 0) {
         that.getInfo()
       }
     })
   },
 
   //更新个人信息
-  updateInfo:function(){
-    if(app.getUserId() === 'test'){
+  updateInfo: function () {
+    if (app.getUserId() === 'test') {
       app.msg('测试号无法更新数据')
       return
     }
-    let _this = this 
+    let _this = this
     wx.showLoading({
       title: '正在更新',
       mask: true
     })
     updateUserInfo().then((res) => {
       app.msg(res.message)
-      if(res.status != 0){
+      if (res.status != 0) {
         return
       }
       _this.getInfo()
